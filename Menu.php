d3617a0a46ff98dc4b0e69939007a7382902154e
LuisCoradi <92060245+LuisCoradi@users.noreply.github.com>
2022-03-04 14:59:10 -0300

    Create Menu.php

---
+++ Menu.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Sistema Video Locadora </title>
+</head>
+<body>
+    <?php
+    include("conexao.php");
+    ?>
+    <p><img src="filme.jpg" width="319" height="289" alt=""/></p>
+</body>
+</html>
