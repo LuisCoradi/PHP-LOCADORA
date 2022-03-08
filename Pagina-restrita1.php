85bea172b9453410a976b0fba2b01baa5e41857a
LuisCoradi <92060245+LuisCoradi@users.noreply.github.com>
2022-03-08 16:32:57 -0300

    Create Pagina-restrita1.php

---
+++ Pagina-restrita1.php
@@ -0,0 +1,16 @@
+<?php 
+include("verifica-usuario.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="Piada.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina de Acesso Restrito - 1</title>
+</head>
+<body>
+ <h1> Esta é a pagina 1 de acesso restrito a usuários cadastrados</h1>   
+</body>
+</html>
