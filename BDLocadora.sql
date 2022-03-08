00854d36e5241acd8c2ba7d2540c90bd20f9b2d7
LuisCoradi <92060245+LuisCoradi@users.noreply.github.com>
2022-03-08 17:28:24 -0300

    Update BDLocadora.sql

--- BDLocadora.sql
+++ BDLocadora.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Tempo de geração: 25-Fev-2022 às 16:11
+-- Tempo de geração: 08-Mar-2022 às 17:27
 -- Versão do servidor: 8.0.28-0ubuntu0.20.04.3
 -- versão do PHP: 7.4.3
 
@@ -45,6 +45,22 @@ INSERT INTO `tbCategorias` (`idCategoria`, `nomeCategoria`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `tbContatos`
+--
+
+CREATE TABLE `tbContatos` (
+  `idContato` int NOT NULL,
+  `nomeContato` varchar(45) DEFAULT NULL,
+  `telefoneContato` varchar(45) DEFAULT NULL,
+  `emailContato` varchar(45) DEFAULT NULL,
+  `cpfContato` varchar(45) DEFAULT NULL,
+  `idadeContato` int DEFAULT NULL,
+  `niverContato` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `tbFilmes`
 --
 
@@ -64,7 +80,26 @@ INSERT INTO `tbFilmes` (`idFilme`, `tituloFilme`, `duracaoFilme`, `valorLocacao`
 (1, 'Exterminador do Futuro', '1:30', '3.50', 1),
 (2, 'Indiana Jones', '2:00', '3.00', 2),
 (3, 'Rambo 2', '2:30', '3.00', 1),
-(4, 'Star Wars', '1:45', '3.00', 3);
+(4, 'Star Wars', '1:45', '3.00', 3),
+(5, 'Batman', '2:30', '3.00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbUsuario`
+--
+
+CREATE TABLE `tbUsuario` (
+  `login_user` varchar(15) NOT NULL,
+  `senha_user` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Extraindo dados da tabela `tbUsuario`
+--
+
+INSERT INTO `tbUsuario` (`login_user`, `senha_user`) VALUES
+('Luis', 'Felipe123');
 
 --
 -- Índices para tabelas despejadas
@@ -77,11 +112,23 @@ ALTER TABLE `tbCategorias`
   ADD PRIMARY KEY (`idCategoria`);
 
 --
+-- Índices para tabela `tbContatos`
+--
+ALTER TABLE `tbContatos`
+  ADD PRIMARY KEY (`idContato`);
+
+--
 -- Índices para tabela `tbFilmes`
 --
 ALTER TABLE `tbFilmes`
   ADD PRIMARY KEY (`idFilme`),
   ADD KEY `idCategoria` (`idCategoria`);
+
+--
+-- Índices para tabela `tbUsuario`
+--
+ALTER TABLE `tbUsuario`
+  ADD PRIMARY KEY (`login_user`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
@@ -94,10 +141,16 @@ ALTER TABLE `tbCategorias`
   MODIFY `idCategoria` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT de tabela `tbContatos`
+--
+ALTER TABLE `tbContatos`
+  MODIFY `idContato` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de tabela `tbFilmes`
 --
 ALTER TABLE `tbFilmes`
-  MODIFY `idFilme` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idFilme` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restrições para despejos de tabelas
