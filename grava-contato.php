f41abb2c16527da58fbde522d9f7db4b2b01f943
LuisCoradi <92060245+LuisCoradi@users.noreply.github.com>
2022-03-08 17:27:32 -0300

    Create grava-contato.php

---
+++ grava-contato.php
@@ -0,0 +1,16 @@
+<?php
+include("conexao.php");
+
+$nomeContato = $_POST["nomeContato"];
+$telefoneContato = $_POST["telefoneContato"];
+$emailContato = $_POST["emailContato"];
+$cpfContato = $_POST["cpfContato"];
+$idadeContato = $_POST["idadeContato"];
+$niverContato = $_POST["niverContato"];
+
+if (isset($nomeContato)== "" or isset($telefoneContato)== "" or
+isset($emailContato)== "" or isset($cpfContato)== "" or
+isset($idadeContato)== "" or isset($niverContato)== ""){
+     echo "Você não preencheu todos os dados"; 
+
+} 
